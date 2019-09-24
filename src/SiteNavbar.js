c3d3492c96c29741d609296f95a8ba8d8798e850
Philip Whitehouse <philip@whiuk.com>
2019-09-24 22:58:06 +0100

    Fix SiteNavbar on mobile

--- src/SiteNavbar.js
+++ src/SiteNavbar.js
@@ -6,38 +6,41 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 class SiteNavbar extends Component {
   render() {
-    return <Navbar bg="dark" variant="dark">
+    return <Navbar bg="dark" variant="dark" expand="sm">
       <LinkContainer to="/">
         <Navbar.Brand>Philip Whitehouse</Navbar.Brand>
       </LinkContainer>
-      <Nav className="mr-auto">
-        <LinkContainer to="/">
-          <Nav.Link>Home</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/blog">
-          <Nav.Link>Blog</Nav.Link>
-        </LinkContainer>
-        <NavDropdown title="Projects" id="collasible-nav-dropdown">
-          <LinkContainer to="/projects/serpentine">
-            <NavDropdown.Item>Serpentine</NavDropdown.Item>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/projects/open-source">
-            <NavDropdown.Item>Open Source</NavDropdown.Item>
+          <LinkContainer to="/blog">
+            <Nav.Link>Blog</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/projects/games">
-            <NavDropdown.Item>Games</NavDropdown.Item>
+          <NavDropdown title="Projects" id="collasible-nav-dropdown">
+            <LinkContainer to="/projects/serpentine">
+              <NavDropdown.Item>Serpentine</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/projects/open-source">
+              <NavDropdown.Item>Open Source</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/projects/games">
+              <NavDropdown.Item>Games</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/projects/history">
+              <NavDropdown.Item>Historical Content</NavDropdown.Item>
+            </LinkContainer>
+          </NavDropdown>
+          <LinkContainer to="/cv">
+            <Nav.Link href="#cv">CV</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/projects/history">
-            <NavDropdown.Item>Historical Content</NavDropdown.Item>
+          <LinkContainer to="/contact">
+            <Nav.Link>Contact</Nav.Link>
           </LinkContainer>
-        </NavDropdown>
-        <LinkContainer to="/cv">
-          <Nav.Link href="#cv">CV</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/contact">
-          <Nav.Link>Contact</Nav.Link>
-        </LinkContainer>
-      </Nav>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>;
   }
 }
